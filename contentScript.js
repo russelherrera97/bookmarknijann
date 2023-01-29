0c75dff9e75c8e04291c0cf14d0dbeec0bff051a
Russ <russelherrera.97@gmail.com>
2023-01-29 18:09:45 +0800

    add utils function

--- contentScript.js
+++ contentScript.js
@@ -1,30 +1,68 @@
 (() => {
   let youtubeLeftControls, youtubePlayer
-  let currentVideo= ''
+  let currentVideo = ''
+  let currentVideoBookmarks = []
   chrome.runtime.onMessage.addListener((obj, sender, response) => {
     const { type, value, videoId } = obj
 
-    if (type === "NEW") {
+    if (type === 'NEW') {
       currentVideo = videoId
       newVideoLoaded()
     }
   })
 
-  const newVideoLoaded = () => {
+  const fetchBookmarks = () => {
+    return new Promise((resolve) => {
+      chrome.storage.sync.get([currentVideo], (obj) => {
+        resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]): [])
+      })
+    })
+  }
+
+  const newVideoLoaded = async () => {
     const bookmarkBtnExists = document.getElementsByClassName('bookmark-btn')[0]
+    currentVideoBookmarks = await fetchBookmarks()
 
     if (!bookmarkBtnExists) {
       const bookmarkBtn = document.createElement('img')
 
       bookmarkBtn.src = chrome.runtime.getURL('assets/bookmark.png')
+      bookmarkBtn.style.width = '20px'
+      bookmarkBtn.style.height = '60%'
+      bookmarkBtn.style.paddingBottom = '10px'
       bookmarkBtn.className = 'ytp-button ' + 'bookmark-btn'
       bookmarkBtn.title = 'Click to bookmark current timestamp'
 
-      youtubeLeftControls = document.getElementsByClassName('ytp-right-controls')[0]
+      youtubeLeftControls =
+        document.getElementsByClassName('ytp-right-controls')[0]
       youtubePlayer = document.getElementsByClassName('video-stream')[0]
 
       youtubeLeftControls.appendChild(bookmarkBtn)
+      bookmarkBtn.addEventListener('click', addNewBookMarkEventHandler)
     }
+  }
+
+  const addNewBookMarkEventHandler = async () => {
+    const currentTime = youtubePlayer.currentTime
+    const newBookmark = {
+      time: currentTime,
+      desc: 'Bookmark at ' + getTime(currentTime),
+    }
+
+    currentVideoBookmarks = await fetchBookmarks()
+
+    chrome.storage.sync.set({
+      [currentVideo]: JSON.stringify(
+        [...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)
+      ),
+    })
+  }
+
+  const getTime = (t) => {
+    var date = new Date(0)
+    date.setSeconds(t)
+
+    return date.toISOString().substr(11, 0)
   }
 
   newVideoLoaded()
